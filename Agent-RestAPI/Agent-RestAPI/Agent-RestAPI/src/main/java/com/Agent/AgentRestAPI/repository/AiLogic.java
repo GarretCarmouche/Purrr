1a18011516215b98001e77e0a418984d8c3fe4e6
Alonso Montelongo <alonsomontelongo88@gmail.com>
2022-09-28 16:25:05 -0500

    commit to checkout main branch

--- Agent-RestAPI/Agent-RestAPI/Agent-RestAPI/src/main/java/com/Agent/AgentRestAPI/repository/AiLogic.java
+++ Agent-RestAPI/Agent-RestAPI/Agent-RestAPI/src/main/java/com/Agent/AgentRestAPI/repository/AiLogic.java
@@ -7,41 +7,22 @@ import org.springframework.stereotype.Component;
 
 import com.Agent.AgentRestAPI.model.Agent;
 import com.Agent.AgentRestAPI.model.Board;
+import com.Agent.AgentRestAPI.model.Wall;
 
 @Component
 public class AiLogic 
 {
     private Agent cat;
+    private HashMap<String,Agent> wallList = new HashMap<>();
     private Board gameBoard= null;// Just temporary until someone works on board class
 
 
 
 public AiLogic()
-{
+{  
     cat = new Agent();
     gameBoard =new Board();
 }
-
-/* 
-public String[] getCurrentLocation()
-{
-    String[] currentLocation= {cat.getQ(),cat.getR(),cat.getS()};
-    return currentLocation;
-}
-*/
-
-
-
-//test json mapping
-public Map<String, String> getAgentLocation() {
-    HashMap<String, String> map = new HashMap<>();
-    map.put("q", cat.getQ());
-    map.put("r", cat.getR());
-    map.put("s", cat.getS());
-    return map;
-}
-
-
 
 
 //test json mapping
@@ -53,6 +34,33 @@ public Map<String, String> sayHello() {
     return map;
 }
 
+
+public Map<String, String> getAgentLocation() {
+    HashMap<String, String> map = new HashMap<>();
+    map.put("q", cat.getQ());
+    map.put("r", cat.getR());
+    map.put("s", cat.getS());
+    return map;
+}
+
+
+//Add wall to list of walls
+public void addWall(String key,Agent wall)
+{
+    
+    wallList.put(key,wall);
+
+}
+
+
+
+public Map<String,Agent> getAllWalls()
+{
+    return wallList;
+
+}
+
+
 //set new location
 public boolean moveCat(String q,String r,String s)
 {
@@ -62,6 +70,9 @@ public boolean moveCat(String q,String r,String s)
     return true;
 
 }
+
+
+
 
 
 
