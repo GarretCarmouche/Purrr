cd280b75d1a99ff9d9671da4f8700e4c92c0ec4e
kdevlin01 <karldevlin01@gmail.com>
2022-10-02 15:37:50 -0500

    Finished GameOver

--- FrontEnd/src/Components/GameOver.jsx
+++ FrontEnd/src/Components/GameOver.jsx
@@ -1,7 +1,18 @@
 import React from "react";
 import '../GameOverStyle.css'
+import { useNavigate } from "react-router-dom";
 
-const gameOver = () =>{
+const GameOver = () =>{
+    const navigate = useNavigate();
 
-    return
-}
+    const handlePlayAgain = () =>{
+        navigate("/")
+    }
+    return (
+        <div className="GOcontainer">
+            <h1>Game Over</h1>
+            <div className="PlayAgain"><button type="button" id="playAgainBTN" onClick={handlePlayAgain}>Play Again</button></div>
+        </div>
+    );
+};
+export default GameOver
