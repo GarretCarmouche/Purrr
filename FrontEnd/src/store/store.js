e214eb5677e72f519e17a7423986b03281accc24
Alonso Montelongo <alonsomontelongo88@gmail.com>
2022-11-19 19:43:35 -0600

    Creating the slice and adding the configering the store

--- FrontEnd/src/store/store.js
+++ FrontEnd/src/store/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import gridReducer from "./girdSlice";
+import boardReducer from "./GameState"
 
 //The store variable is configured and exported here
 export const store = configureStore({
   reducer: {
     grid: gridReducer,
+    boardState: boardReducer,
   },
 });
